2dde36afac0e10f2b1662dea923c46784c1026a3
Anna KOt <sashakot2@gmail.com>
2022-10-29 18:06:16 +0300

    gitignore added

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 const HEADER_HEIGHT_PIXELS = 81;
+const TOTAL_HEIGHT_OF_STICKY_HEADER_PIXELS = 110;
 
 window.addEventListener('scroll', () => {
     Array.from(document.getElementsByClassName('section-grid')).forEach(function (section) {
         let sectionPosition = section.getBoundingClientRect();
-        let sectionHeader = section.getElementsByClassName('section-header');
 
-        if (!sectionHeader.length) {
+        let sectionHeader = section.querySelector('.section-header:not(.sticky)');
+        let sectionHeaderPosition = sectionHeader.getBoundingClientRect();
+        let stickyHeader = sectionHeader.parentElement.querySelector('.section-header.sticky') ?? null;
+
+        let stickyHeaderShouldBeAdded = !stickyHeader
+            && sectionHeaderPosition.top < HEADER_HEIGHT_PIXELS
+            && sectionPosition.bottom >= HEADER_HEIGHT_PIXELS;
+
+        let stickyHeaderShouldBeRemoved = stickyHeader
+            && (sectionHeaderPosition.top >= HEADER_HEIGHT_PIXELS || sectionPosition.bottom < HEADER_HEIGHT_PIXELS);
+
+        let stickyHeaderCreated = false;
+
+        if (stickyHeaderShouldBeAdded) {
+            stickyHeader = sectionHeader.cloneNode(true);
+            stickyHeader.classList.add('sticky');
+            stickyHeaderCreated = true;
+        }
+
+        if (stickyHeaderShouldBeRemoved) {
+            stickyHeader.remove();
+
             return;
         }
 
-        sectionHeader = sectionHeader[0];
+        let stickyHeaderShouldBeRepositioned = stickyHeader
+            && sectionPosition.bottom <= HEADER_HEIGHT_PIXELS + TOTAL_HEIGHT_OF_STICKY_HEADER_PIXELS;
 
-        sectionPosition.top < HEADER_HEIGHT_PIXELS
-            ? sectionHeader.classList.add('sticky')
-            : sectionHeader.classList.remove('sticky');
+        if (stickyHeaderShouldBeRepositioned) {
+            stickyHeader.style.top = `${sectionPosition.bottom - TOTAL_HEIGHT_OF_STICKY_HEADER_PIXELS}px`
+        }
+
+        if (stickyHeaderCreated) {
+            sectionHeader.parentElement.prepend(stickyHeader);
+        }
     })
 })
